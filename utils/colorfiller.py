70dfaf10338d85a9ad75b14e7e7bd97fd83bb57b
Jan Warchoł <jan.warchol@gmail.com>
2016-03-21 13:26:21 +0100

    Utils: update colorfiller and add iterm template

--- utils/colorfiller.py
+++ utils/colorfiller.py
@@ -17,27 +17,31 @@ class Color:
         self.r1 = float(r)/255
         self.g1 = float(g)/255
         self.b1 = float(b)/255
-        self.appleR1 = float(r)/255  # TODO
-        self.appleG1 = float(g)/255  # TODO
-        self.appleB1 = float(b)/255  # TODO
+        self.rs = str(self.r1)
+        self.gs = str(self.g1)
+        self.bs = str(self.b1)
 
 scheme_source = {
-    'bg': {
-        0: (10, 10, 10),
-        1: (100, 0, 0),
-    },
-    'lt_bg': {
-        0: (200, 200, 200),
-        1: (200, 100, 100),
-    },
-    'fg': {
-        0: (211, 212, 213),
-        1: (210, 110, 110),
-    },
-    'lt_fg': {
-        0: (10, 10, 10),
-        1: (100, 0, 0),
-    },
+    'bg':         { 0: ( 31, 54, 73) },
+    'black':      { 0: ( 50, 75, 96) },
+    'br_black':   { 0: (109,140,166) },
+    'fg':         { 0: (164,183,199) },
+    'white':      { 0: (164,183,199) },
+    'br_white':   { 0: (196,216,234) },
+
+    'red':        { 0: (255, 62, 61) },
+    'green':      { 0: (110,185, 43) },
+    'yellow':     { 0: (215,170, 37) },
+    'blue':       { 0: ( 77,137,255) },
+    'magenta':    { 0: (222, 93,253) },
+    'cyan':       { 0: ( 75,200,180) },
+
+    'br_red':     { 0: (255, 76, 72) },
+    'br_green':   { 0: (124,202, 56) },
+    'br_yellow':  { 0: (234,186, 49) },
+    'br_blue':    { 0: ( 92,152,255) },
+    'br_magenta': { 0: (240,108,255) },
+    'br_cyan':    { 0: ( 89,217,196) },
 }
 
 def compile_scheme(scheme):
