8827920a54bb6e74818f731568cf6f7047eb8f87
Franciszek Boehlke <franio.kropka.b@gmail.com>
2016-04-11 21:13:29 +0200

    comment in gen-palette

--- gen-palette.py
+++ gen-palette.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python2
 
+# input: table from the-values (copy-pasted)
+
+# output: some css rules for easy color appliance
+
 import sys
 import re
 
@@ -9,5 +13,5 @@ with open('selenized.css', 'w') as out:
     for line in sys.stdin.readlines():
         m = r.match(line)
         if m:
-            out.write('c [f={0}] {{ color: {1}; }}\n'.format(*m.group(1,2)))
-            out.write('c [b={0}] {{ background-color: {1}; }}\n'.format(*m.group(1,2)))
+            out.write('[f={0}] {{ color: {1}; }}\n'.format(*m.group(1,2)))
+            out.write('[b={0}] {{ background-color: {1}; }}\n'.format(*m.group(1,2)))
