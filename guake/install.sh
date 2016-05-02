6873e24ae98d594f14688e0a286a683a76862f2c
Jan Warchoł <jan.warchol@gmail.com>
2016-05-02 20:39:14 +0200

    Guake: update README, add confirmation to install script

--- guake/install.sh
+++ guake/install.sh
@@ -2,6 +2,22 @@
 
 dir=`dirname $0`
 
+echo
+echo -e "\e[1;39mWarning!\e[0m"
+echo -e "This will permanently overwrite the default profile - there is no undo."
+echo -e "Consider backing up your profile before installing Selenized."
+echo
+echo -n "Are you sure you want to continue? (yes/no) "
+
+read confirmation
+if [[ $(echo $confirmation | tr '[:lower:]' '[:upper:]') != YES ]]
+then
+  echo "ERROR: Confirmation failed -- ABORTING!"
+  exit 1
+fi
+
+echo "Confirmation received -- applying settings"
+
 # palette variant
 VARIANT=${1:-medium}
 
