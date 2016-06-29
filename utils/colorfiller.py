349845b26cf0e206061935625e1d79d4e35ee718
Jan Warchoł <jan.warchol@gmail.com>
2016-06-28 17:59:29 -0700

    Utils: load palette module dynamically
    This allows choosing which palette to apply.

--- utils/colorfiller.py
+++ utils/colorfiller.py
@@ -4,43 +4,46 @@ import os
 import sys
 import re
 import convert
+import importlib
 import selenized_medium
 
-USAGE = """Evaluate templates using selenized palette.
+USAGE = """Evaluate templates using specified palette.
 
-Takes one argument - path to a template file or to a folder that will be
-recursively searched for templates."""
+Takes two arguments - path to a template file or to a folder that will be
+recursively searched for templates and name of the palette module to use."""
 
 TMPL_EXT = '.template'
 
 MARKER_RE = re.compile(r'!!COL(?P<delim>.)(?P<format>.*?)(?P=delim)')
 
-palette = {
-    name: convert.Color(color)
-    for name, color
-    in selenized_medium.palette.iteritems()
-}
+def load_palette(module_name):
+    m = importlib.import_module(module_name)
+    palette = {
+        name: convert.Color(color) for name, color in m.palette.iteritems()
+    }
 
-# Add more attributes that can be used in templates
-for name, color in palette.iteritems():
-    # print "{:<12}{}".format(name, color)
-    color.r  = int(round(color.srgb.rgb_r*255))
-    color.g  = int(round(color.srgb.rgb_g*255))
-    color.b  = int(round(color.srgb.rgb_b*255))
-    color.r1 = color.srgb.rgb_r
-    color.g1 = color.srgb.rgb_g
-    color.b1 = color.srgb.rgb_b
-    color.rs = str(color.srgb.rgb_r)
-    color.gs = str(color.srgb.rgb_g)
-    color.bs = str(color.srgb.rgb_b)
-    color.apple.r = color.apple.rgb_r
-    color.apple.g = color.apple.rgb_g
-    color.apple.b = color.apple.rgb_b
+    # Add more attributes that can be used in templates
+    for name, color in palette.iteritems():
+        # print "{:<12}{}".format(name, color)
+        color.r  = int(round(color.srgb.rgb_r*255))
+        color.g  = int(round(color.srgb.rgb_g*255))
+        color.b  = int(round(color.srgb.rgb_b*255))
+        color.r1 = color.srgb.rgb_r
+        color.g1 = color.srgb.rgb_g
+        color.b1 = color.srgb.rgb_b
+        color.rs = str(color.srgb.rgb_r)
+        color.gs = str(color.srgb.rgb_g)
+        color.bs = str(color.srgb.rgb_b)
+        color.apple.r = color.apple.rgb_r
+        color.apple.g = color.apple.rgb_g
+        color.apple.b = color.apple.rgb_b
 
-def repl(matcher):
-    return matcher.group('format').format(**palette)
+    return palette
 
-def process_template(filepath):
+def process_template(filepath, palette):
+    def repl(matcher):
+        return matcher.group('format').format(**palette)
+
     print "Processing {}...".format(filepath)
     resultpath = filepath[:-(len(TMPL_EXT))]
     with open(filepath, 'r') as ifile, open(resultpath, 'w') as ofile :
@@ -52,15 +55,18 @@ def process_template(filepath):
                 sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print USAGE
         sys.exit()
-    elif os.path.isfile(sys.argv[1]):
-        process_template(sys.argv[1])
+    path = sys.argv[1]
+    palette = load_palette(sys.argv[2])
+
+    if os.path.isfile(path):
+        process_template(path, palette)
     else:
-        for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
+        for dirpath, dirnames, filenames in os.walk(path):
             for filename in filenames:
                 filepath = os.path.join(dirpath, filename)
                 if filepath.endswith(TMPL_EXT):
-                    process_template(filepath)
+                    process_template(filepath, palette)
 
