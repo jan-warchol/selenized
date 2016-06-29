a8a5d73e28891ceda7b6aa64463e6765ac0bf39a
Jan Warchoł <jan.warchol@gmail.com>
2016-06-28 18:04:47 -0700

    Add module with terminator palette

---
+++ utils/terminator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+palette = {
+    "bg":         "#000000",
+    "black":      "#2e3436",
+    "br_black":   "#555753",
+    "fg":         "#aaaaaa",
+    "white":      "#d3d7cf",
+    "br_white":   "#eeeeec",
+                  
+    "red":        "#cc0000",
+    "yellow":     "#c4a000",
+    "green":      "#4e9a06",
+    "cyan":       "#06989a",
+    "blue":       "#3465a4",
+    "magenta":    "#75507b",
+
+    "br_red":     "#ef2929",
+    "br_yellow":  "#fce94f",
+    "br_green":   "#8ae234",
+    "br_cyan":    "#8ae234",
+    "br_blue":    "#729fcf",
+    "br_magenta": "#ad7fa8",
+}
