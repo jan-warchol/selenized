1514d48aa193d896cc1e474fba0ef1ee15094970
Jan Warchoł <jan.warchol@gmail.com>
2016-06-29 17:37:33 -0700

    Initial support for dark-on-light palettes

--- utils/palettes/selenized_base.py
+++ utils/palettes/selenized_base.py
@@ -2,19 +2,23 @@ def generate_palette(background, foreground, br_accent_incr=5):
     bg_l, bg_a, bg_b = background
     fg_l, fg_a, fg_b = foreground
 
+    # for palettes that have dark text on light background we invert lightness
+    # change direction (i.e. "bright" colors are actually darker)
+    direction = -1 if bg_l > fg_l else 1
+
     background_tones = {
-        "bg":       [bg_l,    bg_a,     bg_b    ],
-        "black":    [bg_l+10, bg_a*1.2, bg_b*1.2],
+        "bg":       [bg_l,                bg_a,     bg_b    ],
+        "black":    [bg_l + 10*direction, bg_a*1.2, bg_b*1.2],
     }
 
     content_tones = {
-        "br_black": [fg_l-15, fg_a*1.2, fg_b*1.5],
-        "fg":       [fg_l,    fg_a,     fg_b    ],
-        "white":    [fg_l,    fg_a,     fg_b    ],
-        "br_white": [fg_l+10, fg_a,     fg_b    ],
+        "br_black": [fg_l - 15*direction, fg_a*1.2, fg_b*1.5],
+        "fg":       [fg_l,                fg_a,     fg_b    ],
+        "white":    [fg_l,                fg_a,     fg_b    ],
+        "br_white": [fg_l + 10*direction, fg_a,     fg_b    ],
     }
 
-    accent_base_l = fg_l
+    accent_base_l = fg_l if direction == 1 else fg_l + 16
     accents = {
         "red":      [accent_base_l - 14,  63,  40],
         "orange":   [accent_base_l -  8,  37,  50],
@@ -28,7 +32,7 @@ def generate_palette(background, foreground, br_accent_incr=5):
 
     # bright accents have the same a* b* coords as regular accents
     br_accents = {
-        'br_'+name: [l+br_accent_incr, a, b]
+        'br_'+name: [l + br_accent_incr*direction, a, b]
         for name, [l, a, b]
         in accents.iteritems()
     }
