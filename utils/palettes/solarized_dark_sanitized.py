97de4c13522ed8ed8bce0e1eba4ca72bcb156e8d
Jan Warchoł <jan.warchol@gmail.com>
2016-11-13 19:19:12 -0800

    Fix sanitized versions of solarized
    - for reason unknown to me the darkest color in solarized is by default
      assigned to br_black rather than black
    - in sanitized light version bright colors should actually be darker
      (because everything is inverted)

--- utils/palettes/solarized_dark_sanitized.py
+++ utils/palettes/solarized_dark_sanitized.py
@@ -4,7 +4,7 @@ palette = {
     "bg":         "#002b36",
     "fg":         "#839496",
 
-    "black":      "#073642",
+    "black":      "#002b36",
     "red":        "#dc322f",
     "green":      "#859900",
     "yellow":     "#b58900",
@@ -13,7 +13,7 @@ palette = {
     "cyan":       "#2aa198",
     "white":      "#eee8d5",
 
-    "br_black":   "#002b36",
+    "br_black":   "#073642",
     "br_red":     "#dc322f",
     "br_green":   "#859900",
     "br_yellow":  "#b58900",
