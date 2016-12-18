499cbfd4898954df913551afaa7949e9e1e734e5
Jan Warchoł <jan.warchol@gmail.com>
2016-12-18 10:33:44 +0100

    Selenized light: more saturated background
    I think it just looks better.

--- utils/palettes/selenized_light.py
+++ utils/palettes/selenized_light.py
@@ -3,7 +3,7 @@ import selenized_base
 name = 'Selenized light'
 
 palette = selenized_base.generate_palette(
-    background=(95,  0, 11),
+    background=(95,  0, 12),
     foreground=(45, -6, -6),
     accent_offset=-1,
     br_accent_shift=-5,
