6e0fa224471d02ec1651b9ef77db6a0ae196d4ef
Jan Warchoł <jan.warchol@gmail.com>
2016-12-27 22:41:22 +0100

    More saturated blue
    It's simply better looking.

--- utils/palettes/selenized_base.py
+++ utils/palettes/selenized_base.py
@@ -79,7 +79,7 @@ def generate_palette(
         "yellow":   [accent_base_l - 0.06*accent_l_spread,   6,  65],
         "green":    [accent_base_l - 0.36*accent_l_spread, -37,  53],
         "cyan":     [accent_base_l - 0.12*accent_l_spread, -40,  -4],
-        "blue":     [accent_base_l - 0.84*accent_l_spread,   0, -55],
+        "blue":     [accent_base_l - 0.84*accent_l_spread,   0, -57],
         "violet":   [accent_base_l - 0.66*accent_l_spread,  33, -48],
         "magenta":  [accent_base_l - 0.50*accent_l_spread,  55, -15],
     }
