ec77b96ea4241bafb50a6401222d6160def789d7
Jan Warchoł <jan.warchol@gmail.com>
2016-12-27 20:34:50 +0100

    More dynamic accents in selenized light
    I overdid the lighness limitations for accent colors previously - the
    result was that yellow and cyan were much too dark and therefore ugly.
    It's also harder to tell the hue of an accent color if it's too dark.
    
    With these settings, minimum contrast between accent colors and
    background in selenized light is 37, just as in solarized light
    (compared to 35 in version 0.8.1 and 39.5 in version 0.8.2). I think
    it's a good compromise between good-looking accents and enough contrast.

--- utils/palettes/selenized_light.py
+++ utils/palettes/selenized_light.py
@@ -6,8 +6,8 @@ palette = selenized_base.generate_palette(
     background=(96,  0, 12),
     foreground=(42, -6, -6),
     accent_offset=3,
-    br_accent_shift=2,
-    accent_l_spread=10,
+    br_accent_shift=3,
+    accent_l_spread=15,
     br_bg_extra_saturation=1,
 )
 
