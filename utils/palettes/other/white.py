9f6fbd23d55c110244651a375c511a31aadd0259
Jan Warchoł <jan.warchol@gmail.com>
2016-12-27 21:53:37 +0100

    Add some other interesting selenized variants

---
+++ utils/palettes/other/white.py
@@ -0,0 +1,11 @@
+import selenized_base
+
+# inspired by http://jinja.pocoo.org/docs/dev/templates/
+
+palette = selenized_base.generate_palette(
+    background=(97, 0, 0),
+    foreground=(28, 0, 0),
+    saturation=1.4,
+    accent_offset=12,
+)
+
