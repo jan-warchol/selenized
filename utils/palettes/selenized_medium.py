2bdc189c23991ebd919672049886e6945b39af92
Jan Warchoł <jan.warchol@gmail.com>
2017-01-13 20:22:43 +0100

    Monotones: hues like solarized
    I decided that I want to make the dark and medium versions be warmer and
    have hues of foreground and background exactly like solarized. After
    all, the most important difference from solarized is the overall
    contrast improvement and better accent colors.

--- utils/palettes/selenized_medium.py
+++ utils/palettes/selenized_medium.py
@@ -3,7 +3,7 @@ import selenized_base
 name = 'Selenized medium'
 
 palette = selenized_base.generate_palette(
-    background=(24, -10, -16),
-    foreground=(76,  -6,  -6),
+    background=(24, -13, -13),
+    foreground=(76,  -5,  -2),
 )
 
