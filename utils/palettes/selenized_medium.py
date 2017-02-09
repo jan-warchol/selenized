192f91613da1debe19a49ad5d647593cd0f1a8db
Jan Warchoł <jan.warchol@gmail.com>
2017-02-09 20:42:35 +0100

    Test: decrease saturation of sele med

--- utils/palettes/selenized_medium.py
+++ utils/palettes/selenized_medium.py
@@ -3,7 +3,7 @@ import selenized_base
 name = 'Selenized medium'
 
 palette = selenized_base.generate_palette(
-    background=(24, -13, -13),
+    background=(24, -11, -11),
     foreground=(76,  -5,  -2),
 )
 
