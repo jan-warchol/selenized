f3cf9f69bbd8462301cf6aeafa4de4ef9b7553c6
Jan Warchoł <jan.warchol@gmail.com>
2017-02-09 20:42:35 +0100

    Change lightness and background in selenized medium
    Previous background was slightly too intense and overwhelmed accent
    colors. This should still have enough character while staying, well, in
    the background. Lightness change apparently also helps with that (or
    maybe it just makes some colors more saturated and concrete).

--- utils/palettes/selenized_medium.py
+++ utils/palettes/selenized_medium.py
@@ -3,7 +3,7 @@ import selenized_base
 name = 'Selenized medium'
 
 palette = selenized_base.generate_palette(
-    background=(24, -13, -13),
-    foreground=(76,  -5,  -2),
+    background=(23, -12, -12),
+    foreground=(75,  -5,  -2),
 )
 
