d240d666b71839c9a27eeb234fd6ba87be4d8e07
Jan Warchoł <jan.warchol@gmail.com>
2017-02-09 20:30:57 +0100

    Increase saturation of selenized light
    Somehow the light version appears less saturated than the medium one.
    This effect may be related to the fact that light palette with the same
    nominal contrast as a dark one appears softer (see commit
    258539de843b570d79273aa5a0d616cb07717dd3).

--- utils/palettes/selenized_light.py
+++ utils/palettes/selenized_light.py
@@ -6,6 +6,7 @@ palette = selenized_base.generate_palette(
     background=(96,  0, 12),
     foreground=(42, -6, -6),
     accent_offset=3,
+    saturation=1.05,
     br_accent_shift=3,
     accent_l_spread=14,
     br_bg_extra_saturation=1,
