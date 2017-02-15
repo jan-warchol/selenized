24c9f8483219a401bf92ea2218d57f85c2bd9f26
Jan Warchoł <jan.warchol@gmail.com>
2017-02-15 08:55:04 +0100

    selenized light final

--- utils/palettes/selenized_light.py
+++ utils/palettes/selenized_light.py
@@ -6,9 +6,9 @@ palette = selenized_base.generate_palette(
     background=(96,  0, 12),
     foreground=(42, -6, -6),
     saturation=1.05,
-    accent_offset=1,
+    accent_offset=2,
     br_accent_shift=3,
-    accent_l_spread=16,
+    accent_l_spread=15,
     br_bg_extra_saturation=1,
 )
 
