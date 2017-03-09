27acbb00f0a0da523a80e24c8eef44087529060b
Jan Warchoł <jan.warchol@gmail.com>
2017-03-09 22:17:29 +0100

    Increase accent color spread in white
    I have to admit that light accents (like yellow and cyan) in selenized
    white look much better when they are lighter. The improvement in chroma
    is probably worth the loss of contrast.

--- utils/palettes/other/white.py
+++ utils/palettes/other/white.py
@@ -6,6 +6,7 @@ palette = selenized_base.generate_palette(
     background=(100, 0, 0),
     foreground=(30, 0, 0),
     saturation=1.4,
-    accent_offset=10,
+    accent_offset=5,
+    accent_l_spread=33,
 )
 
