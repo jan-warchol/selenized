04970c274d8c09b958070c214df40d8987074f7e
Jan Warchoł <jan.warchol@gmail.com>
2017-06-30 19:11:09 +0200

    Custom palette for Piotr Kozłowski

---
+++ utils/palettes/other/piotr_kozlowski.py
@@ -0,0 +1,12 @@
+import selenized_base
+
+name = 'Piotr Kozlowski'
+
+palette = selenized_base.generate_palette(
+    background=(97,  0, 8),
+    foreground=(25, -6, -6),
+    saturation=1.4,
+    accent_offset=5,
+    accent_l_spread=30,
+)
+
