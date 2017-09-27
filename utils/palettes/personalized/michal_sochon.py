28dd6fc1c431faeb669556467e43aadd58c65de9
Jan Warchoł <jan.warchol@gmail.com>
2017-09-27 10:41:03 +0200

    Custom palette for Michał Sochoń

---
+++ utils/palettes/personalized/michal_sochon.py
@@ -0,0 +1,10 @@
+import selenized_base
+
+name = "Selenized dla Michala Sochonia"
+
+palette = selenized_base.generate_palette(
+    background=(21, 0, 0),
+    foreground=(91, 0, 0),
+    # br_accent_shift=10,
+)
+
