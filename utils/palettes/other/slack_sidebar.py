4aa9fb56c8901927cb30b2ec1918a151115efe5d
Jan Warchoł <jan.warchol@gmail.com>
2019-01-31 12:00:29 +0100

    Slack: make background just a bit more saturated

--- utils/palettes/other/slack_sidebar.py
+++ utils/palettes/other/slack_sidebar.py
@@ -3,8 +3,8 @@ import selenized_base
 name = 'Selenized for Slack sidebar'
 
 palette = selenized_base.generate_palette(
-    background=(20, -9, -9),
-    foreground=(95, -9, -0),
+    background=(20, -10, -10),
+    foreground=(95, -10,  -0),
     accent_offset=12,
     br_bg_extra_saturation=1.4
 )
