083bf0da5a3a404cd3325973335184d8404dd895
Jan Warchoł <jan.warchol@gmail.com>
2019-08-03 11:29:08 +0200

    Rewrite 8-color preview

--- tests/8-color-preview.sh
+++ tests/8-color-preview.sh
@@ -11,25 +11,28 @@ COMMENT_COLOR=${1:-37}
 
 allcolors() {
     # $1 - modifier (format), $2 - row header, $3 - row comment
+    sequence=(31 "31;1" 33 32 36 34 "34;1" 35)
+
+    reset_colors "$(printf '%6s') "
+    echo -en "\033[${COMMENT_COLOR}m"
+    for ansi_code in ${sequence[*]}; do
+        printf '%5s' "${ansi_code}"
+    done
+
+    echo ""
     reset_colors "$(printf '%6s' $1)_ "
-    for color_code in 1 "1;1" 2 3 4 "4;1" 5 6; do
-        echo -en "\033[${1}${color_code}m${2} "; reset_colors
+    for ansi_code in ${sequence[*]}; do
+        echo -en "\033[${1}${ansi_code}m${2} "; reset_colors
     done
     reset_colors " \033[${COMMENT_COLOR}m${3} \n"
 }
 
 reset_colors "\n"
-echo -en "\033[${COMMENT_COLOR}m" 
-echo -e "        red  orng grn  yel  blue viol pink cyan  \033[0m"
 
-allcolors 3       "text"         "text"
-echo ""
-allcolors "7;3"   "$SPACE_BLOCK" "reverse"
-
-echo "   |\\"
-echo "   | Color code (listed in column header)"
-echo "   Formatting attribute(s)"
+allcolors ""       "text"         "text"
+echo
+allcolors "7;"   "$SPACE_BLOCK" "reverse"
 
 reset_colors "\n"
-echo -e "\033[0m Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+echo -e "\033[0m Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n"
 
