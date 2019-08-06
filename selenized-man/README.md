2b39520f86c0234cc2fa81cc95fd462e4a0ec386
Daniel Gray <dng@disroot.org>
2019-08-06 05:52:34 +0000

    Add selenized-man colored pages

---
+++ selenized-man/README.md
@@ -0,0 +1,18 @@
+Selenized man pages based off [solarized-man](https://github.com/zlsun/solarized-man):
+
+Install:
+
+1. Copy into:
+```
+~/.config/ohmyzsh/custom/plugins/selenized-man
+```
+
+2. Set custom ohmyzsh plugin directory in `.zshrc`
+```
+ZSH_CUSTOM=$HOME/.config/ohmyzsh/custom
+```
+
+3. Set plugin (along with your other plugins)
+```
+plugins=(selenized-man)
+```
---
+++ selenized-man/selenized-man.plugin.zsh
@@ -0,0 +1,34 @@
+if [ "$OSTYPE[0,7]" = "solaris" ]
+then
+    if [ ! -x ${HOME}/bin/nroff ]
+    then
+        mkdir -p ${HOME}/bin
+        cat > ${HOME}/bin/nroff <<EOF
+#!/bin/sh
+if [ -n "\$_NROFF_U" -a "\$1,\$2,\$3" = "-u0,-Tlp,-man" ]; then
+    shift
+    exec /usr/bin/nroff -u\${_NROFF_U} "\$@"
+fi
+#-- Some other invocation of nroff
+exec /usr/bin/nroff "\$@"
+EOF
+chmod +x ${HOME}/bin/nroff
+        fi
+fi
+
+function man() {
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[30;39;100m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[4;32m") \
+        PAGER="${commands[less]:-$PAGER}" \
+        _NROFF_U=1 \
+        GROFF_NO_SGR=1 \
+        PATH=${HOME}/bin:${PATH} \
+    man "$@"
+}
+
