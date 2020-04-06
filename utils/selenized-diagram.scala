3ad4e19b43d1ab7422f047e325ff934ecf466a33
Jan Warchoł <jan.warchol@gmail.com>
2020-04-06 18:18:41 +0200

    Diagrams: format xml in scala code
    Also squareBorder -> lineWidth

--- utils/selenized-diagram.scala
+++ utils/selenized-diagram.scala
@@ -2,50 +2,49 @@ val plotW = 800
 val plotH = 600
 val squareSize = 44
 val squareHalf = squareSize/2.0
-val squareBorder = 3
+val lineWidth = 3
 
 class Color(val name: String, val hexString: String, val luminance: Int)
 
 def genSvg(bgColors: List[Color], fgColors: List[Color]) = {
-    <svg
-       xmlns="http://www.w3.org/2000/svg"
-       xmlns:svg="http://www.w3.org/2000/svg"
-       width={plotW.toString}
-       height={plotH.toString}
-       version="1.1"
-       font-family="Signika, sans"
-       font-size={(squareHalf).toString+"px"}>
-       <style>
+    <svg version="1.1"
+         xmlns="http://www.w3.org/2000/svg"
+         xmlns:svg="http://www.w3.org/2000/svg"
+         width={plotW.toString}
+         height={plotH.toString}
+         font-family="Signika, sans"
+         font-size={(squareHalf).toString+"px"} >
+        <style>
             @font-face {{
-            font-family: 'Signika';
-            font-style: normal;
-            font-weight: 400;
-            src: local('Signika'), local('Signika-Regular'), url(https://fonts.gstatic.com/s/signika/v6/q41y_9MUP_N8ipOH4ORRvw.woff2) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+                font-family: 'Signika';
+                font-style: normal;
+                font-weight: 400;
+                src: local('Signika'), local('Signika-Regular'),
+                  url(https://fonts.gstatic.com/s/signika/v6/q41y_9MUP_N8ipOH4ORRvw.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC,
+                  U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
             }}
             @font-face {{
-            font-family: 'Signika';
-            font-style: normal;
-            font-weight: 700;
-            src: local('Signika-Bold'), url(https://fonts.gstatic.com/s/signika/v6/7M5kxD4eGxuhgFaIk95pBfk_vArhqVIZ0nv9q090hN8.woff2) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+                font-family: 'Signika';
+                font-style: normal;
+                font-weight: 700;
+                src: local('Signika-Bold'),
+                  url(https://fonts.gstatic.com/s/signika/v6/7M5kxD4eGxuhgFaIk95pBfk_vArhqVIZ0nv9q090hN8.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC,
+                  U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
             }}
-       </style>{
-            genBgColors(bgColors.sortBy(x => x.luminance))
-        }{
-            genFgColors(fgColors, bgColors.head)
-    }</svg>
+        </style>
+        { genBgColors(bgColors.sortBy(x => x.luminance)) }
+        { genFgColors(fgColors, bgColors.head) }
+    </svg>
 }
 
 def genBgColors(colors: List[Color]) = {
-    <g transform={"translate(0,"+plotH.toString+") scale(1,-1)"}>{
+    <g transform={"translate(0,"+plotH.toString+") scale(1,-1)"} >{
         for {color <- new Color("#000000", "000000", 0)::colors} yield {
-            <rect
-            style={"fill:#"+color.hexString+";fill-opacity:1;stroke:none"}
-            width={ plotW.toString }
-            height={ plotH.toString }
-            x="0"
-            y={ (color.luminance*plotH/100.0).toString } />
+            <rect x="0" y={ (color.luminance*plotH/100.0).toString }
+                  width={ plotW.toString } height={ plotH.toString }
+                  style={"fill:#"+color.hexString+";fill-opacity:1;stroke:none"} />
         }
     }</g>
 }
@@ -55,27 +54,41 @@ def genFgColors(colors: List[Color], bgColor: Color) = {
         for {(color, i) <- colors.zipWithIndex} yield {
             val xcenter = (i+1)*plotW/(colors.length+1)
             val ycenter = color.luminance*plotH/100.0
-            <g transform={"scale(1,-1) translate("+xcenter.toString+","+ycenter.toString+") scale(1,-1) translate(0, "+plotH.toString+")"}>
-                <!-- group is translated to the center of square with some flipping for easier translation -->
-                <g stroke={"#"+bgColor.hexString} stroke-width={squareBorder.toString}>
-                    <rect
-                        fill={"#"+color.hexString}
-                        stroke={"#"+bgColor.hexString}
-                        stroke-width={squareBorder.toString}
-                        width={squareSize.toString}
-                        height={squareSize.toString}
-                        x={(-squareHalf).toString}
-                        y={(-squareHalf).toString}/>
-                    <line x1={(-squareHalf).toString} y1="0" x2={(squareBorder-squareHalf).toString} y2="0"/>
-                    <line x1={squareHalf.toString} y1="0" x2={(squareHalf-squareBorder).toString} y2="0"/>
+
+            <!-- group is translated to the center of square with some flipping for easier translation -->
+            <g transform={"scale(1,-1)" +
+                          "translate("+xcenter.toString+","+ycenter.toString+")" +
+                          "scale(1,-1)" +
+                          "translate(0, "+plotH.toString+")"} >
+                <g stroke={"#"+bgColor.hexString} stroke-width={lineWidth.toString} >
+                    <rect x={(-squareHalf).toString}
+                          y={(-squareHalf).toString}
+                          width={squareSize.toString}
+                          height={squareSize.toString}
+                          fill={"#"+color.hexString}
+                          stroke={"#"+bgColor.hexString}
+                          stroke-width={lineWidth.toString} />
+
+                    <line x1={(-squareHalf).toString} y1="0"
+                          x2={(lineWidth-squareHalf).toString} y2="0" />
+                    <line x1={squareHalf.toString} y1="0"
+                          x2={(squareHalf-lineWidth).toString} y2="0" />
                 </g>
-                <text x="0" y="0" fill={"#"+bgColor.hexString} text-anchor="middle" dominant-baseline="central">{
-                    color.luminance.toString
-                }</text>
-                <text x={(-squareSize*0.8).toString} y="0" fill={"#"+color.hexString} text-anchor="end" dominant-baseline="central" font-weight="bold" font-size={(0.8*squareSize).toString}
-                    transform={"rotate(-90)"}>{
-                    color.name
-                }</text>
+
+                <text x="0" y="0"
+                      fill={"#"+bgColor.hexString}
+                      text-anchor="middle" dominant-baseline="central" >
+                    { color.luminance.toString }
+                </text>
+
+                <text x={(-squareSize*0.8).toString} y="0"
+                      fill={"#"+color.hexString}
+                      text-anchor="end"
+                      dominant-baseline="central"
+                      font-weight="bold" font-size={(0.8*squareSize).toString}
+                      transform={"rotate(-90)"} >
+                    { color.name }
+                </text>
             </g>
         }
     }</g>
