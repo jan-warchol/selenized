eae65f0ab35a1bdd4b244530b219dc1f7f725aef
Jan Warchoł <jan.warchol@gmail.com>
2022-11-25 21:46:39 +0100

    Very high contrast for presentations on bad projector

---
+++ utils/palettes/other/very_high_contrast.py
@@ -0,0 +1,10 @@
+import selenized_base
+
+name = 'very high contrast'
+
+palette = selenized_base.generate_palette(
+    background=( 0, 0, 0),
+    foreground=(75, 0, 0),
+    accent_l_spread=12,
+)
+
